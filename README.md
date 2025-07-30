7023b9ece3b92f496ab6be07ce0830682c6137ee
manningt <tom@manningetal.com>
2025-07-30 17:44:15 -0400

    add change_message()

---
+++ README.md
@@ -0,0 +1,6 @@
+# epd_10in2g_test.py
+This creates a PIL image and calls epd.display to write it to the e-paper.
+It takes 21 secconds to update the display.
+
+# webserver
+This flask based server displays a single page with buttons to select a message to display on the web page.
--- epd_10in2g_test.py
+++ epd_10in2g_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import sys
-import os
 import time
+import os
 import traceback
 
 import logging
@@ -28,82 +28,71 @@ def find_font_fit(msg):
     # logging.info(f"final font size= {fontsize} for '{msg}'")
     return fontsize
 
-
-try:
-    epd = epd10in2g.EPD()
-    if 1:
-        logging.info("init")
-        epd.init()
-    if 0:
-        logging.info("Clear")
-        epd.Clear()
-
-    closed = True
-    if closed:
+def change_message(top_msg, bottom_msgs):
+    if top_msg.lower() == "closed":
         top_fill_color = epd.RED
         top_text_color = epd.WHITE
+        bottom_fill_color = epd.BLACK
+        bottom_text_color = epd.YELLOW
     else:
         top_fill_color = epd.YELLOW
         top_text_color = epd.BLACK
-
-    # bottom half is always black with white text; width is 960, height is 640
-
-    start_time = datetime.datetime.now()
+        bottom_fill_color = epd.BLACK
+        bottom_text_color = epd.WHITE
+    
+    # width is 960, height is 640
     Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
     # top half is a pie-slice (start and end angles are from 3 o'clock), bottom half is black
     shape = [(0, 0), (epd.width, epd.height)]
     draw.pieslice(shape, start = 180, end = 0, fill = top_fill_color)
-    draw.rectangle(((0, epd.height/2), (epd.width, epd.height)), fill = epd.BLACK)
+    draw.rectangle(((0, epd.height/2), (epd.width, epd.height)), fill = bottom_fill_color)
 
     font180 = ImageFont.truetype(FONT_ITALIC_PATH, 180)
-    msg = "Closed"
-    w = font180.getlength(msg)
-    draw.text(((epd.width-w)/2, 60), msg, font = font180, fill = top_text_color)
+    w = font180.getlength(top_msg)
+    draw.text(((epd.width-w)/2, 60), top_msg, font = font180, fill = top_text_color)
 
-    msg_lines = ("Sorry for the inconvenience", "Our tour guide is unavailable")
     previous_font_size = 0
-    for line in msg_lines:
+    for line in bottom_msgs:
         fontsize = find_font_fit(line)
         lower_msg_font = ImageFont.truetype(FONT_NORMAL_PATH, fontsize)
         w = lower_msg_font.getlength(line)
-        draw.text(((epd.width-w)/2, (epd.height/2 + 25 + previous_font_size)), line, font = lower_msg_font, fill = epd.WHITE)
+        draw.text(((epd.width-w)/2, (epd.height/2 + 25 + previous_font_size)), line, font = lower_msg_font, fill = bottom_text_color)
         previous_font_size = fontsize + 20
-        # could use font.getheight()
-
-
-    end_time = datetime.datetime.now()
-    elapsed_time = end_time - start_time
-    # logging.info(f"Time taken to draw text: {elapsed_time.seconds} seconds {elapsed_time.microseconds} microseconds")
-
-    start_time = datetime.datetime.now()
+            # could use font.getheight()
     buf = epd.getbuffer(Himage)
-    end_time = datetime.datetime.now()
-    elapsed_time = end_time - start_time
-    # logging.info(f"Time taken to getbuffer: {elapsed_time.seconds} seconds {elapsed_time.microseconds} microseconds")
-
+    
     start_time = datetime.datetime.now()
     logging.info("Before display image")
     epd.display(buf)
     end_time = datetime.datetime.now()
     elapsed_time = end_time - start_time
     logging.info(f"Time taken to display: {elapsed_time.seconds} seconds {elapsed_time.microseconds} microseconds")
-    
+
+
+if __name__ == '__main__':
+    try:
+        epd = epd10in2g.EPD()
+        epd.init()
+
+    except Exception as e:
+        logging.info(e)
+        exit()
+
+    try:
+        # change_message("Closed", ("Sorry for the inconvenience", "Our tour guide is unavailable"))
+        # logging.info("Should display closed message")
+        # time.sleep(5)
+        change_message("Open", ("Tour in progress", "Please return at 2 for a tour"))
+        logging.info("Should display Return at 2 message")
+ 
+    except Exception as e:
+        logging.info(e)
+        exit()
+
     '''
-    logging.info("2.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '10in2g.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(3)
-   '''
-     
-    logging.info("Goto Sleep...")
-    epd.sleep()
-    logging.info("Ending with Sleep.")
-        
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd10in2g.epdconfig.module_exit(cleanup=True)
-    exit()
+        logging.info("2.read bmp file")
+        Himage = Image.open(os.path.join(picdir, '10in2g.bmp'))
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(3)
+    '''
