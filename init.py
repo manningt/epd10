5bf46238d228dd239147a79e426822e8d5e3daae
manningt <tom@manningetal.com>
2025-09-12 06:53:06 -0400

    change log level to debug

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ import logging
 def create_app():
     app = Flask(__name__)
     # logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-    logging.basicConfig(level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s : %(message)s')
+    logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s : %(message)s')
     app.logger.debug(f"in create_app()")
     app.register_blueprint(main)
     return app
