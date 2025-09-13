b0d4dfb5ec27dd5b4a023b9fb101fde3bc36da2b
manningt <tom@manningetal.com>
2025-09-13 09:13:35 -0400

    add socketio

--- init.py
+++ init.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from routes import main
 import logging
+from flask_socketio import SocketIO
 
 def create_app():
     app = Flask(__name__)
-    # logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
     logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s : %(message)s')
     app.logger.debug(f"in create_app()")
     app.register_blueprint(main)
+
+    socketio = SocketIO()
+    socketio = SocketIO(app)
+    socketio.init_app(app)
+    # from . import events
+    # app.register_blueprint(events.bp)
+
     return app
