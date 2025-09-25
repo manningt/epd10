29c6deeec6c2c1962ab1911f64b7be615628d3db
manningt <tom@manningetal.com>
2025-09-25 06:48:08 -0400

    add get_current_sleep_duration, spaces from 4 to 3

--- monitor_esp.py
+++ monitor_esp.py
@@ -15,46 +15,90 @@ Requires:
   - sudo apt-get install inotify-tools
   - add to /etc/sudoers:  myusername ALL = (root) NOPASSWD: /home/pi/repos/epd10/monitor_esp.py   - use: sudo visudo
 
+
+if day of week is Fri, Sat, Sun:
+    between 11:30 and 15:10 do short_sleep
+    between 15:20 and (15:20+9) do long_sleep (it should wake at 23:20 & 7:20)
+    between 6:00 and 11:30 do medium_sleep
+
 '''
 
+from enum import Enum
+class SleepDuration(Enum):
+    LONG = 8 * 60 * 60  # 8 hours
+    MEDIUM = 1 * 60 * 60  # 1 hour
+    SHORT = 10 * 60  # 10 minutes
+    VSHORT = 30  # 30 seconds
 
-def check_ftp_log(timeout=10):
+def monitor_ftp_log(timeout=10):
+   cmd = ['sudo', 'inotifywait', '-q', '-e', 'modify', '/var/log/vsftpd.log', '-t', f'{timeout}']
+   process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+   output, errors = process.communicate()
+   if errors:
+      logging.error(f'Error: {errors.decode()}')
+      return None
+   else:
+      cmd = ['sudo', 'tail', '-1', '/var/log/vsftpd.log']
+      process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+      output2, errors2 = process.communicate()
+      if errors2:
+         logging.error(f'Error: {errors2.decode()}')
 
-    cmd = ['sudo', 'inotifywait', '-q', '-e', 'modify', '/var/log/vsftpd.log', '-t', f'{timeout}']
-    process = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    output, errors = process.communicate()
-    if errors:
-        logging.error(f'Error: {errors.decode()}')
-        return None
-    else:
-        cmd = ['sudo', 'tail', '-1', '/var/log/vsftpd.log']
-        process = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        output2, errors2 = process.communicate()
-        if errors2:
-            logging.error(f'Error: {errors2.decode()}')
+      # return (output.decode(), output2.decode())[1]  # return last line of log file
+      return (output.decode(), output2.decode())  # return last line of log file
+   # print(f'{output.decode()=} {errors.decode()=}')
 
-        # return (output.decode(), output2.decode())[1]  # return last line of log file
-        return (output.decode(), output2.decode())  # return last line of log file
-    # print(f'{output.decode()=} {errors.decode()=}')
+
+def get_current_sleep_duration():
+   return_enum = None
+   try:
+      dir_list = next(os.walk('/home/pi/epaper'))[1]
+   except StopIteration:
+      dir_list = []
+   for directory in dir_list:
+      if directory[0] == 't' and directory[1:].isdigit():
+         sleep_seconds = int(directory[1:])
+         match sleep_seconds:
+            case SleepDuration.LONG.value:
+               return_enum = SleepDuration.LONG
+            case SleepDuration.MEDIUM.value:
+               return_enum = SleepDuration.MEDIUM
+            case SleepDuration.SHORT.value:
+               return_enum = SleepDuration.SHORT
+            case SleepDuration.VSHORT.value:
+               return_enum = SleepDuration.VSHORT
+            case _:
+               logging.debug(f'Unknown sleep duration: {sleep_seconds} seconds')
+         if return_enum:
+            break
+   # logging.debug(f'sleep duration not found')
+   return return_enum
+
 
 def main():
-    print('Monitoring /var/log/vsftpd.log for changes...')
-    while True:
-        modified, last_line = check_ftp_log(timeout=10)
-        if modified:
-            print(f'\nLog file modified: {modified.strip()} at {datetime.now()}')
-            print(f'{last_line=}')
-        else:
-            print('. ', end='', flush=True)
+   sleep_duration = get_current_sleep_duration()
+   logging.debug(f'Current sleep duration: {sleep_duration.name}' if sleep_duration else "None")
+   logging.debug('Monitoring /var/log/vsftpd.log for changes...')
+   while True:
+      modified, last_line = monitor_ftp_log(timeout=10)
+      if modified:
+         # print(f'\nLog file modified: {modified.strip()} at {datetime.now()}')
+         # logging.debug(f'{last_line=}')
+         if last_line and ('[pi] OK LOGIN' in last_line):
+            logging.info('File transfer detected! Exiting monitor.')
+            break
+      else:
+         print('. ', end='', flush=True)
 
 
 if __name__ == '__main__':
 
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted - exiting...')
-        try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
+   # day_of_week = datetime.now().weekday()  # Monday is 0 and Sunday is 6
+   try:
+      main()
+   except KeyboardInterrupt:
+      print('Interrupted - exiting...')
+      try:
+         sys.exit(130)
+      except SystemExit:
+         os._exit(130)
